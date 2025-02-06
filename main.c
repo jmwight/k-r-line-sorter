b0b972a13bd7737bbf9df276de1a48e225df296c
John Morgan Wight <john.morgan.wight@proton.me>
2025-02-06 18:40:36 -0500

    Added/tested malloc and strcpy

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "getaline.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "alloc/alloc.h"
+#include "strcpy/strcpy.h"
+#include <unistd.h>
 
 #define MAXLINELEN	1024
 
@@ -8,10 +11,21 @@ main()
 {
 	char *s = calloc(MAXLINELEN, sizeof(char));
 	int i = 0;
+	char *lineptrlist[25];
+	int n = 0;
 	while(i != EOF)
 	{
 		i = getaline(s, MAXLINELEN);
-		printf("%s\n", s);
+		char *p = alloc(i+1); /* i+1 because we need room for null termination character */
+		strcpy(p, s);
+		lineptrlist[n++] = p;
 	}
+
+	int x;
+	for(x = 0; x < n; ++x)
+	{
+		printf("s%d = %s\n", x, lineptrlist[x]);
+	}
+
 	return 0;
 }
