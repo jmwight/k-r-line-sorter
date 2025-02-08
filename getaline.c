d90e32cad65e9c2d5c5e1a91712d11cf90c1a7dd
John Morgan Wight <john.morgan.wight@proton.me>
2025-02-07 22:57:21 -0500

    Fix EOF on last input

--- getaline.c
+++ getaline.c
@@ -7,12 +7,13 @@ int getaline(char *s, int mxlen)
 	for(l = 0; l < mxlen && (*s = getchar()) != EOF && *s != '\n'; ++l, ++s)
 		;
 	if(*s == EOF)
-	{
 		*s = '\0';
-		return EOF;
+	else
+	{
+		*++s = '\0';
+		++l;
 	}
-	*++s = '\0';
-	++l;
+
 	/* we return -2 to indicate buffer overrun */
 	if(l >= mxlen)
 		return -2;
